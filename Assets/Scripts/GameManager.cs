aa7b9158adb03636cb321157c5a167e52184c50e
Alaksiej <forestingel@gmail.com>
2026-07-10 19:15:30 +0200

    Initial scene created.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Overdrunk
+{
+    [AddComponentMenu("Overdrunk/Game Manager")]
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if(Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+    }
+}
