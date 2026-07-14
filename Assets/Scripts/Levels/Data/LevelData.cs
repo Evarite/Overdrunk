e1af27f122d704de957f4683fd28269cd8d91290
pixaut <116499977+pixaut@users.noreply.github.com>
2026-07-14 21:30:22 +0300

    delete setters

--- Assets/Scripts/Levels/Data/LevelData.cs
+++ Assets/Scripts/Levels/Data/LevelData.cs
@@ -11,8 +11,8 @@ namespace Overdrunk.Level
         [SerializeField] private int _levelNumber = 0;
         [SerializeField] private Image _preview = null;
 
-        public int LevelNumber {get => _levelNumber; set => _levelNumber = value;}
-        public Image Preview {get => _preview; set => _preview = value;}
+        public int LevelNumber {get => _levelNumber;}
+        public Image Preview {get => _preview;}
 
 
         [Header("Stars cost")]
@@ -20,9 +20,9 @@ namespace Overdrunk.Level
         [SerializeField] private int _secondStarCost = 200;
         [SerializeField] private int _thirdStarCost = 300;
         
-        public int FirstStarCost {get => _firstStarCost; set => _firstStarCost = value;}
-        public int SecondStarCost {get => _secondStarCost; set => _secondStarCost = value;}
-        public int ThirdStarCost {get => _thirdStarCost; set => _thirdStarCost = value;}
+        public int FirstStarCost {get => _firstStarCost;}
+        public int SecondStarCost {get => _secondStarCost;}
+        public int ThirdStarCost {get => _thirdStarCost;}
 
 
     }
