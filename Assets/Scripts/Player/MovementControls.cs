ae1d9644470b61662894f7fd119ef6986082054e
Dia-chanterelle <salix.chanterelle@gmail.com>
2026-07-14 21:41:15 +0300

    Character Controls (#1)
    Added WASD player controls.

---
+++ Assets/Scripts/Player/MovementControls.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Overdrunk.Player
+{
+    [AddComponentMenu("Overdrunk/Player/Movement Controller")]
+    [RequireComponent(typeof(Rigidbody))]
+    public class MovementControls : MonoBehaviour
+    {
+        private Rigidbody _rb;
+
+        [field: SerializeField] public float Speed { get; set; } = 10f;
+
+        private Vector2 _moveInput;
+        private PlayerCharacterInputActions _inputAction;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _inputAction = new PlayerCharacterInputActions();
+        }
+
+        private void OnEnable()
+        {
+            _inputAction.Player.Enable();
+
+            _inputAction.Player.Move.performed += Move_performed;
+            _inputAction.Player.Move.canceled += Move_performed;
+
+        }
+        private void OnDisable()
+        {
+            _inputAction.Player.Move.performed -= Move_performed;
+            _inputAction.Player.Move.canceled -= Move_performed;
+
+            _inputAction.Player.Disable();
+        }
+
+        private void Move_performed(InputAction.CallbackContext obj)
+        {
+            _moveInput = obj.ReadValue<Vector2>();
+        }
+
+        private void FixedUpdate()
+        {
+            Vector3 _moveInput3 = new Vector3(_moveInput.x, _rb.linearVelocity.y, _moveInput.y);
+            _rb.linearVelocity = _moveInput3.normalized * Speed;
+        }
+
+    }
+}
