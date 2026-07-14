c04fa5db481261f484238c1eb443561397c6f382
pixaut <116499977+pixaut@users.noreply.github.com>
2026-07-14 21:43:03 +0300

    Level Data Container (#2)
    LevelData and LevelPlayerData created.

---
+++ Assets/Scripts/Levels/Data/LevelData.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Overdrunk.Level
+{
+    [CreateAssetMenu(fileName = "New Level", menuName = "Overdrunk/Levels/Level Data")]
+    public class LevelData : ScriptableObject
+    {
+        
+        [Header("Level Basic Data")]
+        [SerializeField] private int _levelNumber = 0;
+        [SerializeField] private Image _preview = null;
+
+        public int LevelNumber {get => _levelNumber;}
+        public Image Preview {get => _preview;}
+
+
+        [Header("Stars cost")]
+        [SerializeField] private int _firstStarCost = 100;
+        [SerializeField] private int _secondStarCost = 200;
+        [SerializeField] private int _thirdStarCost = 300;
+        
+        public int FirstStarCost {get => _firstStarCost;}
+        public int SecondStarCost {get => _secondStarCost;}
+        public int ThirdStarCost {get => _thirdStarCost;}
+
+
+    }
+}
