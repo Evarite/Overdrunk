81351fe7223f073b81bc88e81d0498086a32d237
pixaut <116499977+pixaut@users.noreply.github.com>
2026-07-14 21:04:36 +0300

    additing player's level data

---
+++ Assets/Scripts/Levels/Data/LevelPlayerData.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+
+
+namespace Overdrunk.Level{
+
+    [System.Serializable]
+    public class LevelPlayerData
+    {
+        
+        [Header("Records")]
+        [SerializeField] private int _starsRecord = 0;
+        [SerializeField] private int _pointsRecord = 0;
+
+        public int StarsRecord {get => _starsRecord; set => _starsRecord = value;}
+        public int PointsRecord {get => _pointsRecord; set => _pointsRecord = value;}
+
+    }
+
+}
