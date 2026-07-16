8d4923fc90078ecb0aeadb307e1ef141428a7b71
pixaut <116499977+pixaut@users.noreply.github.com>
2026-07-16 14:22:47 +0300

    4 vehicle controls (#3)
    Vehicle Controls created.

---
+++ Assets/Scripts/Vehicle/VehicleMovementControls.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Overdrunk.Vehicle
+{
+    [AddComponentMenu("Overdrunk/Vehicle/Vehicle Movement Controls")]
+    [RequireComponent(typeof(Rigidbody))]
+    public class VehicleMovementControls : MonoBehaviour
+    {
+        private Rigidbody _rb;
+
+        [Header("Move speed")]
+        [SerializeField]
+        private float _speed = 10f;
+        public float Speed { get => _speed; set => _speed = value; }
+        private Vector2 _moveInput;
+        private InputActions _inputAction;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _inputAction = new InputActions();
+        }
+
+        private void OnEnable()
+        {
+            _inputAction.Vehicle.Enable();
+
+            _inputAction.Vehicle.Move.performed += MovePerformed;
+            _inputAction.Vehicle.Move.canceled += MovePerformed;
+        }
+        private void OnDisable()
+        {
+            _inputAction.Vehicle.Move.performed -= MovePerformed;
+            _inputAction.Vehicle.Move.canceled -= MovePerformed;
+
+            _inputAction.Vehicle.Disable();
+        }
+
+        private void MovePerformed(InputAction.CallbackContext obj)
+        {
+            _moveInput = obj.ReadValue<Vector2>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            Vector2 _normalizedInput = Speed * Vector2.ClampMagnitude(_moveInput, 1f);
+
+            _rb.linearVelocity = new Vector3(
+                _normalizedInput.x,
+                _rb.linearVelocity.y,
+                _normalizedInput.y
+            );
+        }
+
+    }
+}
